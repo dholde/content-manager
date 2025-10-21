9b9c08aca99f7cde16628cf83bddf3a04bccb543
Denis Holdeew <hold.denis@gmail.com>
2025-10-21 22:12:03 +0200

    Add tenant entity

---
+++ src/main/java/com/dehold/contentmanager/tenant/model/Tenant.java
@@ -0,0 +1,69 @@
+package com.dehold.contentmanager.tenant.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Table("tenant")
+public class Tenant {
+
+    @Id
+    private UUID id;
+    private String name;
+    private String identifier;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Tenant() {
+    }
+
+    public Tenant(UUID id, String name, String identifier, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.identifier = identifier;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
