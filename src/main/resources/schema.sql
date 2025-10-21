148fdbd4246790a4c7df6d5fd249b2746c89c03a
Denis Holdeew <hold.denis@gmail.com>
2025-10-21 22:57:53 +0200

    Persistence setup

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tenant (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    identifier VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
