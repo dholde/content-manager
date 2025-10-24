76db982f7ed0225a13446e65ecaeb689bfed2b28
Denis Holdeew <hold.denis@gmail.com>
2025-10-24 13:00:29 +0200

    Add custom error response

--- src/main/java/com/dehold/contentmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dehold/contentmanager/exception/GlobalExceptionHandler.java
@@ -7,22 +7,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleEntityNotFoundException(
+    public ResponseEntity<CustomErrorResponse> handleEntityNotFoundException(
             EntityNotFoundException ex, WebRequest request) {
 
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", Instant.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("error", ex.getMessage());
-        body.put("path", request.getDescription(false).replace("uri=", ""));
+        CustomErrorResponse errorResponse = new CustomErrorResponse(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
 
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
