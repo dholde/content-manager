a937612707b1075f71b378cd87f6b719817665aa
Denis Holdeew <hold.denis@gmail.com>
2025-10-28 18:27:24 +0100

    Improve tests

--- src/test/java/com/dehold/contentmanager/validation/web/dto/ValidationResponseTest.java
+++ src/test/java/com/dehold/contentmanager/validation/web/dto/ValidationResponseTest.java
@@ -1,7 +1,10 @@
 package com.dehold.contentmanager.validation.web.dto;
 
+import com.dehold.contentmanager.validation.result.ValidationError;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +13,11 @@ class ValidationResponseTest {
 
     @Test
     void givenEqualValidationResponses_whenEquals_thenReturnTrue() {
-        ValidationResultDto validationResult1 = new ValidationResultDto(true, null);
-        ValidationResultDto validationResult2 = new ValidationResultDto(true, null);
+        ValidationError error1 = new ValidationError("FIELD_1", "Error message 1");
+        ValidationError error2 = new ValidationError("FIELD_2", "Error message 2");
+
+        ValidationResultDto validationResult1 = new ValidationResultDto(true, List.of(error1, error2));
+        ValidationResultDto validationResult2 = new ValidationResultDto(true, List.of(error1, error2));
 
         ValidationResponse response1 = new ValidationResponse("BlogPost", validationResult1);
         ValidationResponse response2 = new ValidationResponse("BlogPost", validationResult2);
@@ -21,8 +27,11 @@ class ValidationResponseTest {
 
     @Test
     void givenDifferentValidationResponses_whenEquals_thenReturnFalse() {
-        ValidationResultDto validationResult1 = new ValidationResultDto(true, null);
-        ValidationResultDto validationResult2 = new ValidationResultDto(false, null);
+        ValidationError error1 = new ValidationError("FIELD_1", "Error message 1");
+        ValidationError error2 = new ValidationError("FIELD_2", "Error message 2");
+
+        ValidationResultDto validationResult1 = new ValidationResultDto(true, List.of(error1, error2));
+        ValidationResultDto validationResult2 = new ValidationResultDto(false, List.of(error1));
 
         ValidationResponse response1 = new ValidationResponse("BlogPost", validationResult1);
         ValidationResponse response2 = new ValidationResponse("BlogPost", validationResult2);
