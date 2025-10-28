7b05a59d7b8b9b95d0d69a1cec31b2adc81cc922
Denis Holdeew <hold.denis@gmail.com>
2025-10-28 13:09:24 +0100

    Test skeleton

--- src/test/java/com/dehold/contentmanager/validation/web/ValidationControllerIntegrationTest.java
+++ src/test/java/com/dehold/contentmanager/validation/web/ValidationControllerIntegrationTest.java
@@ -1,5 +1,24 @@
 package com.dehold.contentmanager.validation.web;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ValidationControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void givenValidBlogPost_whenValidate_thenReturnsSuccess() {
+
+    }
 
 }
