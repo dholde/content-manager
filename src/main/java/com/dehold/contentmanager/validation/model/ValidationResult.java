19a1415e9cdbbba5a9d26aab7705e5b92d3577a3
Denis Holdeew <hold.denis@gmail.com>
2025-10-29 09:50:34 +0100

    Add method overload for compatibility

--- src/main/java/com/dehold/contentmanager/validation/model/ValidationResult.java
+++ src/main/java/com/dehold/contentmanager/validation/model/ValidationResult.java
@@ -1,22 +1,31 @@
 package com.dehold.contentmanager.validation.model;
 
 import java.util.List;
+import java.util.UUID;
 
 public class ValidationResult {
+    private final String contentType;
+    private final UUID contentId;
     private final boolean isValid;
     private final List<ValidationError> errors;
 
-    private ValidationResult(boolean isValid, List<ValidationError> errors) {
+    private ValidationResult(String contentType, UUID contentId, boolean isValid, List<ValidationError> errors) {
+        this.contentType = contentType;
+        this.contentId = contentId;
         this.isValid = isValid;
         this.errors = errors;
     }
 
     public static ValidationResult valid() {
-        return new ValidationResult(true, List.of());
+        return valid("", null);
+    }
+
+    public static ValidationResult valid(String contentType, UUID contentId) {
+        return new ValidationResult(contentType, contentId, true, List.of());
     }
 
     public static ValidationResult invalid(List<ValidationError> errors) {
-        return new ValidationResult(false, errors);
+        return new ValidationResult(null, null, false, errors);
     }
 
     public boolean isValid() {
