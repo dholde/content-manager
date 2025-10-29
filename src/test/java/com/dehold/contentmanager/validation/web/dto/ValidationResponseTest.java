f30621f3ba989b4203c2bad1bc988a9b7fbe2c91
Denis Holdeew <hold.denis@gmail.com>
2025-10-29 10:16:04 +0100

    ADapt ValidationREsponseTest

--- src/test/java/com/dehold/contentmanager/validation/web/dto/ValidationResponseTest.java
+++ src/test/java/com/dehold/contentmanager/validation/web/dto/ValidationResponseTest.java
@@ -1,10 +1,13 @@
 package com.dehold.contentmanager.validation.web.dto;
 
+import com.dehold.contentmanager.content.blogpost.model.BlogPost;
+import com.dehold.contentmanager.content.customersupport.model.SupportRequest;
 import com.dehold.contentmanager.validation.model.ValidationError;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,8 +19,12 @@ class ValidationResponseTest {
         ValidationError error1 = new ValidationError("FIELD_1", "Error message 1");
         ValidationError error2 = new ValidationError("FIELD_2", "Error message 2");
 
-        ValidationResultDto validationResult1 = new ValidationResultDto(true, List.of(error1, error2));
-        ValidationResultDto validationResult2 = new ValidationResultDto(true, List.of(error1, error2));
+        String contentType = BlogPost.class.getSimpleName();
+        UUID contentId = UUID.randomUUID();
+
+        ValidationResultDto validationResult1 = new ValidationResultDto(contentType, contentId, true, List.of(error1,
+                error2));
+        ValidationResultDto validationResult2 = new ValidationResultDto(contentType, contentId,true, List.of(error1, error2));
 
         ValidationResponse response1 = new ValidationResponse("BlogPost", validationResult1);
         ValidationResponse response2 = new ValidationResponse("BlogPost", validationResult2);
@@ -30,8 +37,12 @@ class ValidationResponseTest {
         ValidationError error1 = new ValidationError("FIELD_1", "Error message 1");
         ValidationError error2 = new ValidationError("FIELD_2", "Error message 2");
 
-        ValidationResultDto validationResult1 = new ValidationResultDto(true, List.of(error1, error2));
-        ValidationResultDto validationResult2 = new ValidationResultDto(false, List.of(error1));
+        String contentType = BlogPost.class.getSimpleName();
+        UUID contentId = UUID.randomUUID();
+
+        ValidationResultDto validationResult1 = new ValidationResultDto(contentType, contentId, true, List.of(error1,
+                error2));
+        ValidationResultDto validationResult2 = new ValidationResultDto(contentType, contentId, false, List.of(error1));
 
         ValidationResponse response1 = new ValidationResponse("BlogPost", validationResult1);
         ValidationResponse response2 = new ValidationResponse("BlogPost", validationResult2);
@@ -41,10 +52,16 @@ class ValidationResponseTest {
 
     @Test
     void givenDifferentContentTypes_whenEquals_thenReturnFalse() {
-        ValidationResultDto validationResult = new ValidationResultDto(true, null);
+        String contentType1 = BlogPost.class.getSimpleName();
+        String contentType2 = SupportRequest.class.getSimpleName();
+        UUID contentId1 = UUID.randomUUID();
+        UUID contentId2 = UUID.randomUUID();
 
-        ValidationResponse response1 = new ValidationResponse("BlogPost", validationResult);
-        ValidationResponse response2 = new ValidationResponse("Article", validationResult);
+        ValidationResultDto validationResult1 = new ValidationResultDto(contentType1, contentId1, true, null);
+        ValidationResultDto validationResult2 = new ValidationResultDto(contentType2, contentId2, true, null);
+
+        ValidationResponse response1 = new ValidationResponse("BlogPost", validationResult1);
+        ValidationResponse response2 = new ValidationResponse("Article", validationResult2);
 
         assertNotEquals(response1, response2);
     }
